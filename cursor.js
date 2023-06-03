66270611f16d93fe0c3f7d1e6b89ad423f229437
TJ Klimoski <tjklimoski@gmail.com>
2023-06-03 19:45:55 -0400

    Improved smoothness of cursor when scrolling

--- cursor.js
+++ cursor.js
@@ -4,7 +4,7 @@ cursor.classList.add("cursor");
 
 //add cursor to document when user's mouse enters page
 document.addEventListener("mouseenter", () => {
-  document.body.appendChild(cursor);
+  document.body.prepend(cursor);
 });
 
 //remove cursor from document when user's mouse leaves page
@@ -14,23 +14,27 @@ document.addEventListener("mouseleave", () => {
 
 document.addEventListener("mousemove", (e) => {
   //set cursor position on page
-  cursor.style.setProperty("--top", `${e.pageY}px`);
-  cursor.style.setProperty("--left", `${e.pageX}px`);
+  setCursorPosition(e);
 
   //check if on selectable element
   isClickable(e);
 });
 
+function setCursorPosition(e) {
+  cursor.style.setProperty("--y", `${e.y}px`);
+  cursor.style.setProperty("--x", `${e.x}px`);
+}
+
 function isClickable(e) {
   switch (e.target.nodeName.toLowerCase()) {
     case "a":
-      cursor.style.setProperty("scale", "1.5");
+      cursor.style.setProperty("--scale", "1.5");
       break;
     case "button":
       animateBtnFill(e);
       break;
     default:
-      cursor.style.setProperty("scale", "1");
+      cursor.style.removeProperty("--scale");
       return;
   }
 }
